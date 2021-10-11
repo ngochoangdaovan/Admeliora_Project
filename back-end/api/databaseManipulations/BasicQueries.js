25e90034f4450802d7f390e1c643793cf6a37a4d
hoang.dao23062001@gmail.com <hoang.dao23062001@gmail.com>
2021-10-12 02:19:17 +0700

    add structure by hoang dao

---
+++ back-end/api/databaseManipulations/BasicQueries.js
@@ -0,0 +1,16 @@
+
+
+class BasicQueries {
+    constructor (db){
+        this.db = db;
+    }
+
+    async findAll() {};
+    async findByID() {}
+    async delete() { };
+    async create() { };
+    async update() { };
+
+
+}
+
