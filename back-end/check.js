ba95aa1db795b3881403f6a2399d784a702184dd
hoang.dao23062001@gmail.com <hoang.dao23062001@gmail.com>
2021-11-01 14:54:01 +0700

    merge by hoang dao

--- back-end/check.js
+++
@@ -1,7 +0,0 @@
-let start = new Date("2021-10-30 19:07:00")
-let end = new Date()
-
-
-// let timediff = end.getMinutes() - start.getMinutes() 
-
-console.log((end.getTime() - start.getTime())/1000 )
